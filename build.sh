893974ec2062782dac5dee236dc67d47fd78d8e8
Jamie Hewland <jhewland@gmail.com>
2015-12-14 14:25:57 +0200

    buildscript: Quote all the things
    * Also check sha256sum without creating a file

--- build.sh
+++ build.sh
@@ -16,32 +16,31 @@ function download_release_file() {
 echo "Downloading signature files..."
 SHA_FILE="${PROJECT}_${VERSION}_SHA256SUMS"
 SHA_SIG_FILE="$SHA_FILE.sig"
-download_release_file $SHA_FILE
-download_release_file $SHA_SIG_FILE
+download_release_file "$SHA_FILE"
+download_release_file "$SHA_SIG_FILE"
 
 echo "Verifying $SHA_FILE signature..."
-gpg --verify $SHA_SIG_FILE
+gpg --verify "$SHA_SIG_FILE"
 
 ZIP_FILE="${PROJECT}_${VERSION}_${PLATFORM}.zip"
-if [ ! -f $ZIP_FILE ]; then
-    download_release_file $ZIP_FILE
+if [ ! -f "$ZIP_FILE" ]; then
+    download_release_file "$ZIP_FILE"
 else
     echo "File $ZIP_FILE found, not downloading."
 fi
 
 echo "Verifying $ZIP_FILE hash..."
-grep $ZIP_FILE $SHA_FILE > sha256sum.txt
-sha256sum -c sha256sum.txt
+grep "$ZIP_FILE" "$SHA_FILE" | sha256sum -c -
 
 # Hack sideloader to not build things in /var/praekelt - install files straight into the package directory
-PACKAGE_DIR=$WORKSPACE/package
-mkdir -p $PACKAGE_DIR
-mkdir -p $PACKAGE_DIR$INSTALL_DIR
+PACKAGE_DIR="$WORKSPACE/package"
+mkdir -p "$PACKAGE_DIR"
+mkdir -p "$PACKAGE_DIR$BIN_DIR"
 
 echo "Extracting $ZIP_FILE contents to $INSTALL_DIR in package..."
-unzip -qo -d $PACKAGE_DIR$INSTALL_DIR $ZIP_FILE
+unzip -qo -d "$PACKAGE_DIR$INSTALL_DIR" "$ZIP_FILE"
 
 # Create misc empty directories
 for path in $EMPTY_DIRS; do
-    mkdir -p $PACKAGE_DIR$path
+    mkdir -p "$PACKAGE_DIR$path"
 done
